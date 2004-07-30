c66939cb21739dfc38e538c710bb185e6d74f3fd
kjrose <kjrose@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-07-30 16:15:08 +0000

    .htaccess should not be there, TODO.txt isn't even ours, and ChangeLog.txt isn't ours and has never been updated. Time to do some cleanup.
    git-svn-id: http://open.nit.ca/svn/trunk/gracefultavi@6086 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- template/common.php
+++ template/common.php
@@ -44,6 +44,7 @@ function template_common_prologue($args)
 <link rel="STYLESHEET" href="<?php print $StyleScript; ?>" type="text/css">
 <link rel="SHORTCUT ICON" href="<?=$shortcutIcon?>">
 <title><?php print $args['title'] . ' - ' . htmlspecialchars($WikiName); ?></title>
+<?php require_once('javascript.php'); ?>
 </head>
 
 <body>
