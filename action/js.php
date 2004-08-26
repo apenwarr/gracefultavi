bef094e9b509ca3da51a002b487a02061b8dfb0a
mich <mich@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-08-26 21:44:10 +0000

    fixed silly thing kjrose didn't do! ;)
    git-svn-id: http://open.nit.ca/svn/trunk/gracefultavi@6888 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- action/js.php
+++ action/js.php
@@ -1,13 +1,13 @@
 <?php
-// $Id: js.php,v 1.1 2004/08/26 20:44:49 kjrose Exp $
+// $Id: js.php,v 1.2 2004/08/26 21:44:10 mich Exp $
 
 // This function emits the current template's stylesheet.
 function action_style()
 {
     global $HTTP_GET_VARS;
 
-    if ($HTTP_GET_VARS('file') == "tabsort")
+    if ($HTTP_GET_VARS['file'] == "tabsort")
         require('js/tabsort.js');
-    else if ($HTTP_GET_VARS('file') == "script")
+    else if ($HTTP_GET_VARS['file'] == "script")
         require('js/script.js');
 }
