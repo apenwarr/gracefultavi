deb176a292c1b96a83020f6cf1c1d1ac53ce6096
mich <mich@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2007-03-19 19:32:39 +0000

    Added debug info for db queries
    git-svn-id: http://open.nit.ca/svn/trunk/gracefultavi@11588 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- lib/db.php
+++ lib/db.php
@@ -27,7 +27,19 @@ class WikiDB
     global $ErrorDatabaseQuery;
 
     if(!($qid = mysql_query($text, $this->handle)))
-      die("Query: $text<br>" . $ErrorDatabaseQuery);
+    {
+      if (0)
+      {
+        die("$ErrorDatabaseQuery<br><br>" .
+            "Query: $text<br>" .
+            'MySQL error: ' . mysql_error($this->handle) .
+            '<pre>'.print_r(debug_backtrace(), true).'</pre>');
+      }
+      else
+      {
+        die($ErrorDatabaseQuery);
+      }
+    }
 
     return $qid;
   }
